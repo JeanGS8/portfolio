85571cebbd4037a988fe943da36e350cd900a488
JeanGS8 <jean118877@hotmail.com>
2024-08-29 17:18:48 -0300

    fix carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,16 +1,17 @@
-import { ChangeDetectionStrategy, Component, Input, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, ChangeDetectionStrategy, Component, Input, signal } from '@angular/core';
 import { IConhecimentos } from 'app/interfaces/IConhecimentos.interface';
 import { IProjetos } from 'app/interfaces/IProjetos.interface';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CarouselComponent {
+export class CarouselComponent implements AfterViewInit {
   public conhecimentos = signal<IConhecimentos[]>([]);
   
   @Input() set inputConhecimentos(conhecimentos: IConhecimentos[]) {
@@ -22,53 +23,53 @@ export class CarouselComponent {
   @Input() set inputProjetos(projetos: IProjetos[]){
     this.projetos.set(projetos);
   }
-
+  
   ngAfterViewInit(): void {
-    setTimeout(() => {
-      // Conhecimentos
-      $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav'
-      });
+    // Conhecimentos
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider-nav'
+    });
 
-      $('.slider-nav').slick({
-        autoplay: true,
-        autoplaySpeed: 1500,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: false,
-        centerMode: true,
-        focusOnSelect: true,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              autoplaySpeed: 1000,
-              slidesToShow: 1,
-            }
+    $('.slider-nav').slick({
+      autoplay: true,
+      autoplaySpeed: 1500,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3
           }
-        ]
-      });
-      
-      // Projetos
-      $('.single-item').slick();
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            autoplaySpeed: 1000,
+            slidesToShow: 1,
+          }
+        }
+      ]
+    });
 
+    
+    // Projetos
+    setTimeout(() => {
+      $('.single-item').slick();
       $('.single-mini').slick({
         dots: true,
         arrows: false,
         autoplay: true,
         autoplaySpeed: 1500
       });
-    }, 150);
+    }, 300);
   }
 }
